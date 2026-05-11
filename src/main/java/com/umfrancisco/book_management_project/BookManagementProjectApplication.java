3e73c0f6308c89ec8f01fea4f3fed3e7679ca54e
umfrancisco <francis.guitler@gmail.com>
2026-05-11 13:40:17 -0300

    First commit

---
+++ src/main/java/com/umfrancisco/book_management_project/BookManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.umfrancisco.book_management_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umfrancisco/book_management_project/BookManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umfrancisco.book_management_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
