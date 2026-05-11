0fa811a06acf8ad74afe2f43f5d76fd903c01da9
umfrancisco <francis.guitler@gmail.com>
2026-05-11 13:56:59 -0300

    Add book service

---
+++ src/main/java/com/umfrancisco/book_management_project/book/service/BookService.java
@@ -0,0 +1,22 @@
+package com.umfrancisco.book_management_project.book.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.umfrancisco.book_management_project.book.model.Book;
+import com.umfrancisco.book_management_project.book.repository.BookRepository;
+
+@Service
+public class BookService {
+	
+	private final BookRepository repository;
+	
+	public BookService(BookRepository repository) {
+		this.repository = repository;
+	}
+	
+	public List<Book> getAllBooks() {
+		return repository.findAll();
+	}
+}
